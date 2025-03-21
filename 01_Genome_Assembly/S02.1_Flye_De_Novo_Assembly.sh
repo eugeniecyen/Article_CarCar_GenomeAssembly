9bbfaac00f66bb6e4bf70fbc2b0735e0503b6e3c
eugeniecyen <e.yen@qmul.ac.uk>
2025-03-21 10:50:49 +0000

    Replace genome assembly scripts

--- 01_Genome_Assembly/S02.1_Flye_De_Novo_Assembly.sh
+++ 01_Genome_Assembly/S02.1_Flye_De_Novo_Assembly.sh
@@ -17,7 +17,7 @@
 # Prep environment
 module load python/3.8.5
 
-TOOL=/data/home/btx902/privatemodules/Flye/bin
+FLYE=/data/home/btx902/privatemodules/Flye/bin/flye
 DATA_DIR=/data/SBCS-EizaguirreLab/Turtle_Genome/02_Quality_Control/03_Trimmed_Reads/02_NanoFilt
 OUT_DIR=/data/SBCS-EizaguirreLab/Turtle_Genome/03_De_Novo_Assembly/Flye_Assembly/asm_cov_40
 
@@ -26,8 +26,7 @@ echo "Starting assembly: "`date`
 
 # --asm-coverage 40 recommended for initial disjointig assembly with lower coverage
 # to reduce memory consumption for large genome assemblies
-
-python $TOOL/flye \
+python $FLYE \
   -t ${NSLOTS} \
   --nano-raw $DATA_DIR/turtle_porechop_q8minlen500.fastq.gz \
   --out-dir $OUT_DIR \
--- 01_Genome_Assembly/S03.1_Polish_Medaka.sh
+++ 01_Genome_Assembly/S03.1_Polish_Medaka.sh
@@ -19,15 +19,15 @@ module load anaconda3
 conda activate medaka
 
 READS_DIR=/data/SBCS-EizaguirreLab/Turtle_Genome/02_Quality_Control/03_Trimmed_Reads/02_NanoFilt
-ASSEMBLY_DIR=/data/SBCS-EizaguirreLab/Turtle_Genome/04_Polished_Assembly/Racon/Wtdbg2/round_4
-OUT_DIR=/data/SBCS-EizaguirreLab/Turtle_Genome/04_Polished_Assembly/Racon_Medaka/Wtdbg2/round_4
+ASSEMBLY_DIR=/data/SBCS-EizaguirreLab/Turtle_Genome/04_Polished_Assembly/Medaka
+OUT_DIR=/data/SBCS-EizaguirreLab/Turtle_Genome/04_Polished_Assembly/Medaka
 
 echo "Started: "`date`
 
 # Run Medaka polishing
 medaka_consensus \
 -i $READS_DIR/turtle_porechop_q8minlen500.fastq.gz \
--d $ASSEMBLY_DIR/turtle_wtdbg2_racon_x4.fasta \
+-d $ASSEMBLY_DIR/turtle_flye_medaka.fasta \
 -o $OUT_DIR \
 -m r941_prom_high_g4011 \
 -t ${NSLOTS}
